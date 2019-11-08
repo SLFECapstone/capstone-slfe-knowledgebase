3797af64291da7a0ba1f4723830830e77baf800d
musicalfisch <qfischer@asu.edu>
2019-11-08 11:41:12 -0700

    made admin page, admin only

--- client/src/components/Pages/tempAdminPage.jsx
+++ client/src/components/Pages/tempAdminPage.jsx
@@ -1,18 +1,43 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Navbar from '../PageComponents/Navbar'
 import Footer from '../PageComponents/Footer'
 
 class tempAdminPage extends Component {
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  };
+
+  constructor(props) {
+		super(props);
+  }
+
   componentDidMount() {
     document.title = "SLFE - Temp Admin Page";
   }
 
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+
     return (
       <div >
+        { isAuthenticated ? (
+        <div >
+          { (user.role == 'Administrator') ? (
+          <div >
+            Welcome Administrator
+          </div>):(<div >Unauthorized User</div>)}
+        </div>):(<div >Unauthenticated User</div>)}
       </div>
     )
   }
 }
 
-export default tempAdminPage;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps
+)(tempAdminPage);
