7f30868e1efc47a0d4a53c8e07cde384266fed98
musicalfisch <qfischer@asu.edu>
2019-11-14 12:49:16 -0700

    added edit profile template, but cannot get text to load into template

--- client/src/components/PageComponents/Profile/index.jsx
+++ client/src/components/PageComponents/Profile/index.jsx
@@ -11,8 +11,25 @@ class Profile extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {editMode: false};
+
+    const { profile } = props.profileData;
+    //Having trouble loading profile data in constructor, yet need info to initialize text fields with existing data.
+    this.state = {
+      editMode: false,
+      firstname: 'Profile',
+      lastname: profile.last_name,
+      organization: '',
+      position: '',
+      email: ''
+    };
+
     this.handleEditClick = this.handleEditClick.bind(this);
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangeOrganization = this.onChangeOrganization.bind(this);
+    this.onChangePosition = this.onChangePosition.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
   }
 
   handleEditClick(){
@@ -21,8 +38,43 @@ class Profile extends Component {
     });
   }
 
-  componentDidMount() {
-    
+  onChangeFirstName(e) {
+		this.setState({
+			firstname: e.target.value
+		});
+	}
+
+	onChangeLastName(e) {
+		this.setState({
+			lastname: e.target.value
+		});
+  }
+  
+  onChangeOrganization(e) {
+		this.setState({
+			organization: e.target.value
+		});
+  }
+  
+  onChangePosition(e) {
+		this.setState({
+			position: e.target.value
+		});
+  }
+  
+  onChangeEmail(e) {
+		this.setState({
+			email: e.target.value
+		});
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		
+		const apiCall = this.props.loginFunc({ username: this.state.username, password: this.state.password });
+		apiCall.then(data => {
+			this.props.history.push('/');
+		});
   }
 
   render() {
@@ -42,13 +94,102 @@ class Profile extends Component {
     }
 
     // console.log(user);
+    // { expression ? body : alternate body } means eval boolean expression, if true then body else alternate body.
     return (
       <div>
         { profile ? (
           <div>
             {this.state.editMode && isOwnProfile ? (
             <div>
-              In Edit Mode on your own profile.
+              <div id="profile_form" className="form_input">
+                <div className="profile-form">
+					      <form onSubmit={this.onSubmit}>
+						    <h2>Welcome to your Profile { userName } ([Edit Mode])</h2>
+						    <div className="form-group">
+							    <div className="input-group">
+								    <span className="input-group-addon">
+									    <i className="fa fa-user"/> First Name 
+								    </span>
+                    <input
+                    type="text"
+                    className="form-control"
+                    name="firstname"
+                    value={this.state.firstname}
+                    required="required"
+                    onChange={this.onChangeFirstName}
+                    />
+							    </div>
+						    </div>
+						    <div className="form-group">
+							    <div className="input-group">
+								    <span className="input-group-addon">
+									    <i className="fa fa-user-o"/> Last Name 
+								    </span>
+								    <input
+									  type="text"
+									  className="form-control"
+									  name="lastname"
+									  value={this.state.lastname}
+									  required="required"
+                    onChange={this.onChangeLastName}
+								    />
+							    </div>
+						    </div>
+                <div className="form-group">
+							    <div className="input-group">
+								    <span className="input-group-addon">
+									    <i className="fa fa-sitemap"/> Organization 
+								    </span>
+								    <input
+									  type="text"
+									  className="form-control"
+									  name="organization"
+									  value={this.state.organization}
+									  required="required"
+                    onChange={this.onChangeOrganization}
+								    />
+							    </div>
+						    </div>
+                <div className="form-group">
+							    <div className="input-group">
+								    <span className="input-group-addon">
+									    <i className="fa fa-briefcase"/> Position 
+								    </span>
+								    <input
+									  type="text"
+									  className="form-control"
+									  name="position"
+									  value={this.state.position}
+									  required="required"
+                    onChange={this.onChangePosition}
+								    />
+							    </div>
+						    </div>
+                <div className="form-group">
+							    <div className="input-group">
+								    <span className="input-group-addon">
+									    <i className="fa fa-envelope"/> E-mail 
+								    </span>
+								    <input
+									  type="text"
+									  className="form-control"
+									  name="email"
+									  value={this.state.email}
+									  required="required"
+                    onChange={this.onChangeEmail}
+								    />
+							    </div>
+						    </div>
+						    <div className="form-group">
+							    <button
+								  type="submit"
+								  className="btn btn-primary btn-block btn-lg">
+									Save Changes
+							    </button>
+						    </div>
+					      </form>
+				        </div>
+			        </div>
             </div>) : (
             <div>
               { isOwnProfile ? (
@@ -59,7 +200,7 @@ class Profile extends Component {
               </div>
               ) : (<h2>{ userName }, welcome to { profile.username }'s Profile</h2>)}
               <body>
-                <div>Name: { profile.first_name ? profile.first_name + " " + profile.last_name : "" }</div>
+                <div>Name: { profile.first_name && profile.last_name ? profile.first_name + " " + profile.last_name : "" }</div>
                 <div>Organization: { profile.organization ? profile.organization : "" }</div>
                 <div>Position: { profile.position ? profile.position : "" } </div>
                 <div>Email: { profile.email_address ? profile.email_address : "" } </div>
